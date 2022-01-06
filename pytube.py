c99623ea851f9d5250004d6a06910f87bfccce39
usmarmanalu <usmarmnl99@gmail.com>
2022-01-06 07:31:47 +0700

    unduh video minifield & tambah paragraf

--- pytube.py
+++ pytube.py
@@ -8,9 +8,12 @@ video = list(enumerate(videos))
 for i in video:
     print(i)
     
+    
+print(" ")
 print("pengaturan durasi dan layout video : ")
 dnoption = int(input("masukkan pengaturan : "))
 dnvideo = videos[dnoption]
 dnvideo.download()
+
 
 print("dwonload sukses")
